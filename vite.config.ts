84d6d92b47a646280c5c161c77a21d307fc371c8
Vedat Aydın <vedataydin2365@gmail.com>
2025-02-13 12:47:52 +0300

    new commit added

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import themePlugin from "@replit/vite-plugin-shadcn-theme-json";
-import path from "path";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default defineConfig({
+  base: '/client/',
   plugins: [
     react(), 
     runtimeErrorOverlay(), 
