a6c379df8bab60f6626de2a89a75964a43d98223
Vedat Aydın <vedataydin2365@gmail.com>
2025-02-15 12:45:22 +0300

    Tailwind ve CSS konfigurasyonunu düzelt

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,20 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        border: 'hsl(var(--border) / <alpha-value>)',
-        background: 'hsl(var(--background) / <alpha-value>)',
-        foreground: 'hsl(var(--foreground) / <alpha-value>)',
-      },
-      fontFamily: {
-        sans: ['var(--font-sans)']
-      }
-    },
-  },
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: { extend: {} },
   plugins: [],
 }
