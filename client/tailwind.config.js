22d27373e3aa09fdca3e647378d08d75d1e1d237
Vedat Aydın <vedataydin2365@gmail.com>
2025-02-15 12:38:06 +0300

    Tailwind CSS konfigurasyonunu detaylandır

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -6,8 +6,13 @@ module.exports = {
   ],
   theme: {
     extend: {
-      borderColor: {
-        border: 'hsl(var(--border))',
+      colors: {
+        border: 'hsl(var(--border) / <alpha-value>)',
+        background: 'hsl(var(--background) / <alpha-value>)',
+        foreground: 'hsl(var(--foreground) / <alpha-value>)',
+      },
+      fontFamily: {
+        sans: ['var(--font-sans)']
       }
     },
   },
